f6345e714e8db22ba3afc85ba01265c0c63b5489
Kirk Easterson <kirk.easterson@gmail.com>
2022-12-04 07:24:24 +0100

    Cleaned up day 2

--- python/02/01/01.py
+++ python/02/01/01.py
@@ -7,51 +7,52 @@ shape_points = {
     "Z": 3,
 }
 
+
 def calc_outcome_points(opp, me):
     if shape_points[opp] == shape_points[me]:
         return 3  # draw
     match opp:
         case "A":
-            if me == 'Y':
+            if me == "Y":
                 return 6
         case "B":
-            if me == 'Z':
+            if me == "Z":
                 return 6
         case "C":
-            if me == 'X':
+            if me == "X":
                 return 6
 
     return 0
 
 
-
 def calculate_hand(opp, me):
-    outcome_points = calc_outcome_points(opp,me)
+    outcome_points = calc_outcome_points(opp, me)
     return shape_points[me] + outcome_points
+
 
 def get_appropriate_hand(opp, res):
     match res:
         case "X":  # lose
-            if opp == 'A':
-                return 'Z'
-            elif opp == 'B':
-                return 'X'
+            if opp == "A":
+                return "Z"
+            elif opp == "B":
+                return "X"
             else:
-                return 'Y'
+                return "Y"
         case "Y":  # draw
-            if opp == 'A':
-                return 'X'
-            elif opp == 'B':
-                return 'Y'
+            if opp == "A":
+                return "X"
+            elif opp == "B":
+                return "Y"
             else:
-                return 'Z'
+                return "Z"
         case "Z":  # win
-            if opp == 'A':
-                return 'Y'
-            elif opp == 'B':
-                return 'Z'
+            if opp == "A":
+                return "Y"
+            elif opp == "B":
+                return "Z"
             else:
-                return 'X'
+                return "X"
 
 
 def p1():
@@ -60,15 +61,14 @@ def p1():
 
     total_points = 0
     for round in c:
-        if round == '':
+        if round == "":
             continue
         moves = round.split(" ")
         round_points = calculate_hand(moves[0], moves[1])
-        print('round points: ', round_points)
+        print("round points: ", round_points)
         total_points += round_points
 
     print(total_points)
-
 
 
 def p2():
@@ -77,12 +77,12 @@ def p2():
 
     total_points = 0
     for round in c:
-        if round == '':
+        if round == "":
             continue
         moves = round.split(" ")
-        me = get_appropriate_hand(moves[1])
+        me = get_appropriate_hand(moves[0], moves[1])
         round_points = calculate_hand(moves[0], me)
-        print('round points: ', round_points)
+        print("round points: ", round_points)
         total_points += round_points
 
     print(total_points)
